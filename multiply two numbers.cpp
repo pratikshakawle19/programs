00b07eb5b8bf9cf52dc31dbe764732aa915069b0
pratikshakawle19 <74823453+pratikshakawle19@users.noreply.github.com>
2020-12-04 21:37:31 +0530

    Add files via upload

---
+++ multiply two numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double firstNumber, secondNumber, productOfTwoNumbers;
+	cout<< "Enters two numbers: ";
+	cin >> firstNumber >> secondNumber;
+	productOfTwoNumbers = firstNumber * secondNumber;
+	cout << "product =" << productOfTwoNumbers;
+	return 0;
+}
